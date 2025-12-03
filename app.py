45b2abb47a78f1f8e4dbefe25b4e59c8df463640
muffindud <ccatlabuga2002@gmail.com>
2025-12-03 19:15:01 +0200

    swich to file up/down

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from subprocess import run
 
 from dotenv import dotenv_values
 from OpenSSL import crypto
-from flask import Flask, request
+from flask import Flask, request, send_file
 
 
 app = Flask(__name__)
@@ -11,46 +11,40 @@ config = dotenv_values(".env")
 REMOTE_HOST = config["REMOTE_HOST"]
 REMOTE_PORT = config["REMOTE_PORT"]
 
-CERT_PATH = config["CERT_PATH"]
-CA_PATH = config["CA_PATH"]
-TC_PATH = config["TC_PATH"]
-
-
 def crete_cert(cn: str, req: str) -> str:
+    config = open("template.ovpn", "r").read()
+
     with open(f"/etc/openvpn/server/easy-rsa/pki/reqs/{cn}.req", "w") as f:
         f.write(req)
 
     run(["/etc/openvpn/server/easy-rsa/easyrsa", "--batch", "sign-req","client", cn], cwd="/etc/openvpn/server/easy-rsa")
 
-    inline = open(f"/etc/openvpn/server/easy-rsa/pki/inline/private/{cn}.inline", "r").read()
-
-    return inline
-
-
-def create_config(cn: str, req: str) -> str:
-    config = open("template.ovpn", "r").read()
-    inline = crete_cert(cn, req)
-
-    # Replace placeholders in the template
     config = config.replace("%REMOTE_HOST%", REMOTE_HOST)
     config = config.replace("%REMOTE_PORT%", REMOTE_PORT)
     config = config.replace("%INLINE%", inline)
 
-    return config
+    inline = open(f"/etc/openvpn/server/easy-rsa/pki/inline/private/{cn}.inline", "r").read()
+
+    path = f"./temp/{cn}.ovpn"
+
+    with open(path, "w") as f:
+        f.write(config)
+
+    return path
+
+
+def create_config(cn: str, req: str) -> str:
+    path = crete_cert(cn, req)
+    return send_file(path, as_attachment=True)
 
 
 @app.route("/generate", methods=["POST"])
 def generate_vpn_config():
-    data = request.get_json()
+    data = request.files['file']
 
-    if not "key" in data:
-        return {"error": "Missing 'key' in request body"}, 400
-    if not "cn" in data:
-        return {"error": "Missing 'cn' in request body"}, 400
+    if data.filename == '':
+        return {"error": "No selected file"}, 400
 
-    key = data["key"]
-    cn = data["cn"]
-
-    config = create_config(cn, key)
+    config = create_config(data.filename.split('.')[0], data.read().decode('utf-8'))
 
     return config, 200
